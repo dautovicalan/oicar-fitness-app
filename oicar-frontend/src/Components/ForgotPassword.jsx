67c8dff3271cfa2f4b02061d01b4d7a6cca4f90a
Karlo Hruškovec <khrusko@racunarstvo.hr>
2023-04-05 23:24:51 +0200

    Use yup validation in ForgotPassword page

--- oicar-frontend/src/Components/ForgotPassword.jsx
+++ oicar-frontend/src/Components/ForgotPassword.jsx
@@ -1,5 +1,6 @@
 import * as React from "react";
-import { useState, useEffect } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -14,141 +15,90 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-import { emailValid } from "../Utils/FormValidation";
-
-function Copyright(props) {
-  return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
-    >
-      {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        FitPal
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
-
 const theme = createTheme();
 
-export default function LoginForm() {
-  const initialValues = { email: "", password: "" };
-  const [formValues, setFormValues] = useState(initialValues);
-  const [errors, setErrors] = useState({});
-  const [isSubmit, setIsSubmit] = useState(false);
+const RecoverAccountSchema = Yup.object().shape({
+	email: Yup.string()
+		.email("Invalid email format")
+		.required("Email is required"),
+});
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormValues({ ...formValues, [name]: value });
-  };
+const RecoverAccount = () => {
+	const initialValues = { email: "" };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setErrors(validate(formValues));
-    setIsSubmit(true);
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
-  };
+	const handleSubmit = (values) => {
+		console.log(values);
+	};
 
-  useEffect(() => {
-    console.log(errors);
-    if (Object.keys(errors).length == 0 && isSubmit) {
-      console.log(formValues);
-    }
-  }, [errors]);
+	return (
+		<>
+			<ThemeProvider theme={theme}>
+				<Box sx={{ height: "77vh" }}>
+					<Container component="main" maxWidth="xs">
+						<CssBaseline />
 
-  const validate = (values) => {
-    const errors = {};
-    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    if (!values.email) {
-      errors.email = "Email is required!";
-    } else if (!regex.test(values.email)) {
-      errors.email = "This is not a valid email format!"
-    }
-    if (!values.password) {
-      errors.password = "Password is required!";
-    }
-    return errors;
-  };
+						<Box
+							sx={{
+								marginTop: 8,
+								display: "flex",
+								flexDirection: "column",
+								alignItems: "center",
+							}}
+						>
+							<Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+								<LockOutlinedIcon />
+							</Avatar>
+							<Typography component="h1" variant="h5">
+								Recover account
+							</Typography>
+							<Formik
+								initialValues={initialValues}
+								validationSchema={RecoverAccountSchema}
+								onSubmit={handleSubmit}
+							>
+								{(formik) => (
+									<Box sx={{ minWidth: "45vh" }}>
+										<Form noValidate onSubmit={formik.handleSubmit}>
+											<Field
+												as={TextField}
+												margin="normal"
+												required
+												fullWidth
+												id="email"
+												label="Email Address"
+												name="email"
+												autoComplete="email"
+												autoFocus
+											/>
+											<ErrorMessage name="email">
+												{(msg) => <p style={{ color: "red" }}>{msg}</p>}
+											</ErrorMessage>
+											<Button
+												type="submit"
+												fullWidth
+												variant="contained"
+												sx={{ mt: 3, mb: 2 }}
+												disabled={!formik.isValid}
+											>
+												Send recovery link
+											</Button>
+											<Grid container>
+												<Grid item>
+													<Link href="/login" variant="body2">
+														{"Back to login"}
+													</Link>
+												</Grid>
+											</Grid>
+										</Form>
+									</Box>
+								)}
+							</Formik>
+						</Box>
+					</Container>
+				</Box>
+			</ThemeProvider>
+		</>
+	);
+};
 
-  return (
-    <>
-
-      <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
-      <ThemeProvider theme={theme}>
-        <Container component="main" maxWidth="xs">
-          <CssBaseline />
-          <Box sx={{ height: "77vh" }}>
-            <Box
-              sx={{
-                marginTop: 8,
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
-            >
-              <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-                <LockOutlinedIcon />
-              </Avatar>
-              <Typography component="h1" variant="h5">
-                Recover account
-              </Typography>
-              <Box
-                component="form"
-                onSubmit={handleSubmit}
-                noValidate
-                sx={{ mt: 1, width: '100%'}}
-              >
-                <TextField
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                  autoFocus
-                  value={formValues.email}
-                  onChange={handleChange}
-                />
-                <p style={{ color: "red" }}>{errors.email}</p>
-
-
-                {/* <FormControlLabel
-                       control={<Checkbox value="remember" color="primary" />}
-                     label="Remember me"
-                    /> */}
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  
-                  sx={{ mt: 3, mb: 2 }}
-                >
-                  Send recovery link
-                </Button>
-                <Grid container>
-                  <Grid item>
-                    <Link href="/login" variant="body2">
-                      {"Back to login"}
-                    </Link>
-                  </Grid>
-                </Grid>
-              </Box>
-            </Box>
-            <Copyright sx={{ mt: 8, mb: 4 }} />
-          </Box>
-        </Container>
-      </ThemeProvider>
-
-    </>
-  );
-}
+export default RecoverAccount;
