3e4931b5138d97b97975b02e064f5ae99e2b8fe9
Mario Poljaković <mpolja1@racunarstvo.hr>
2023-04-12 22:10:27 +0200

    resetPassword

--- oicar-backend/oicar-ApiServices/Controllers/AccountController.cs
+++ oicar-backend/oicar-ApiServices/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using Domain.ErrorModel;
 using Domain.Models;
 using FitPal_Models.Dto;
 using FitPal_Models.JsonModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Repository;
 
@@ -10,6 +11,7 @@ namespace oicar_ApiServices.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class AccountController : ControllerBase
     {
         private readonly IRepositoryManager _repository;
@@ -22,6 +24,7 @@ namespace oicar_ApiServices.Controllers
         }
 
         [HttpPost("Login")]
+        [AllowAnonymous]
         public async Task<IActionResult> Login(UserLoginInput userLogin)
         {
             if (await _repository.User.CheckLogin(userLogin.Email, userLogin.Password))
@@ -29,7 +32,7 @@ namespace oicar_ApiServices.Controllers
                 User? user = await _repository.User.GetUserByEmail(userLogin.Email);
                 return Ok(_mapper.Map<UserDto>(user));
             }
-              
+
             return Forbid();
         }
 
@@ -40,11 +43,12 @@ namespace oicar_ApiServices.Controllers
 
             if (user is null)
                 return NotFound(new HttpError("User does not exist"));
-            
+
             return Ok(user);
         }
 
         [HttpPost("Register")]
+        [AllowAnonymous]
         public async Task<IActionResult> Register(UserRegisterInput userInput)
         {
             if (await _repository.User.IsEmailExist(userInput.Email))
@@ -54,6 +58,39 @@ namespace oicar_ApiServices.Controllers
             var newUserDto = _mapper.Map<UserDto>(newUser);
 
             return Ok(newUserDto);
+        }
+
+        [HttpPost("ForgotPassword")]
+        [AllowAnonymous]
+        public async Task<IActionResult> ForgotPassword(string email)
+        {
+            if (!await _repository.User.IsEmailExist(email))
+                return BadRequest(new HttpError("Wrong email"));
+
+            string resetCode = (new Random()).Next(100000, 999999).ToString();
+            await _repository.User.SaveResetPasswordCode(email, resetCode);
+
+            return Ok();
+        }
+
+        [HttpPost("ChangePassword")]
+        [AllowAnonymous]
+        public async Task<IActionResult> ForgotPassword(ChangePasswordInput changePassword)
+        {
+            // Get references
+            string email = changePassword.Email;
+            string code = changePassword.Code;
+            string password = changePassword.Password;
+
+            if (!await _repository.User.IsEmailExist(email))
+                return BadRequest(new HttpError("User doesn't exist"));
+
+            User? user = await _repository.User.ChangePassword(email, code, password);
+
+            if (user == null)
+                return BadRequest(new HttpError("Wrong code"));
+
+            return Ok();
         }
     }
 }
