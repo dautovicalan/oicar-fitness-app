e023f34e7f5401f5ee9047a7fcfe834b24b94d21
Mario Poljaković <mpolja1@racunarstvo.hr>
2023-04-16 11:28:45 +0200

    workout models

---
+++ oicar-backend/Domain/Model/Exercise.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Model
+{
+    public class Exercise
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string GifUrl { get; set; }
+        public int BodyPartId { get; set; }
+        public virtual BodyPart BodyPart { get; set; }
+        public int EquipmentId { get; set; }
+        public virtual Equipment Equipment { get; set; }
+        public int TargetId { get; set; }
+        public virtual TargetMuscle TargetMuscle { get; set; }
+
+    }
+}
