d322af42b8799797eace5085feb6288bcb20d296
Konichwa1998 <karlomarosini14@gmail.com>
2023-04-16 20:22:11 +0200

    Profile and meal plan initial design added (no actions)

--- oicar-frontend/src/Components/MealPlan.jsx
+++ oicar-frontend/src/Components/MealPlan.jsx
@@ -1,7 +1,63 @@
 import React from "react";
+import { Box } from "@mui/material";
+import Paper from "@mui/material";
+import '../Styles/MealPlanStyle.css'
 
 const MealPlan = () => {
-  return <div>MealPlan</div>;
+
+  const myList = [
+		{ id: 1, mealName: "Carrot", Calories: 30},
+		{ id: 2, mealName: "Chicken", Calories: 30},
+		{ id: 3, mealName: "Apple", Calories: 30},
+		{ id: 4, mealName: "Strawberry", Calories: 30},
+		{ id: 5, mealName: "Blackberry", Calories: 30},		
+		{ id: 5, mealName: "Blackberry", Calories: 30},		
+		{ id: 5, mealName: "Blackberry", Calories: 30},		
+		{ id: 5, mealName: "Blackberry", Calories: 30},		
+	];
+
+
+
+  return (
+    <div>
+			<div style={{ display: "flex", flexDirection: "row" }}>
+				<Box
+					sx={{
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+						justifyContent: "center",
+						width: "50%",
+						m: 1,
+						p: 1,
+					}}
+				>
+          <h1>Meals</h1>
+				</Box>
+				<Box
+					sx={{
+						display: "flex",
+						width: "100%",
+						m: 1,
+						p: 1,
+					}}
+				>
+					<div className="item-row">
+						{myList.map((item) => {
+							return (
+								<div className="item">
+									<div className="item-box">
+										<h3>{item.mealName}</h3>
+										<p>Calories: {item.Calories} cal.</p>
+									</div>
+								</div>
+							);
+						})}
+					</div>
+				</Box>
+			</div>
+		</div>
+  )
 };
 
 export default MealPlan;
--- oicar-frontend/src/Components/UserProfile.jsx
+++ oicar-frontend/src/Components/UserProfile.jsx
@@ -1,7 +1,203 @@
 import React from "react";
+import { Avatar, Container } from "@mui/material";
+import { Box, Button } from "@material-ui/core";
+import "../Styles/UserProfile.css";
 
 const UserProfile = () => {
-  return <div>UserProfile</div>;
+  const user = {
+    id: 1,
+    picture: "https://static.independent.co.uk/2022/08/11/16/Andrew%20Tate.jpg",
+    name: "Test User",
+    goal: "My goal",
+    weight: 90,
+    height: 190,
+    newsletter: "Subscribed",
+  };
+
+  return (
+    <div>
+      <Container sx={{justifyContent:'center', alignItems: 'center', display: 'flex'}}>
+        <h1>Profile</h1>
+      </Container>
+      <Container
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Box>
+          <Avatar
+            alt="Test user"
+            sx={{
+              width: 250,
+              height: 250,
+              boxShadow: "0px 8px 10px rgba(0,0,0,0.3)",
+            }}
+            src={user.picture}
+          />
+        </Box>
+      </Container>
+      <Container
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "row",
+          alignItems: "center",
+          width: "30%",
+        }}
+        className="info-container"
+      >
+        <Container
+          sx={{
+            display: "flex",
+            justifyContent: "flex-end",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <h2>Name: </h2>
+        </Container>
+        <Container
+          sx={{
+            display: "flex",
+            justifyContent: "flex-start",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <h2>{user.name}</h2>
+        </Container>
+      </Container>
+      <Container
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "row",
+          alignItems: "center",
+          width: "30%",
+        }}
+        className="info-container"
+      >
+        <Container
+          sx={{
+            display: "flex",
+            justifyContent: "flex-end",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <h2>Goal: </h2>
+        </Container>
+        <Container
+          sx={{
+            display: "flex",
+            justifyContent: "flex-start",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <h2>{user.goal}</h2>
+        </Container>
+      </Container>
+      <Container
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "row",
+          alignItems: "center",
+          width: "30%",
+        }}
+        className="info-container"
+      >
+        <Container
+          sx={{
+            display: "flex",
+            justifyContent: "flex-end",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <h2>Weight: </h2>
+        </Container>
+        <Container
+          sx={{
+            display: "flex",
+            justifyContent: "flex-start",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <h2>{user.weight} kg</h2>
+        </Container>
+      </Container>
+      <Container
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "row",
+          alignItems: "center",
+          width: "30%",
+        }}
+        className="info-container"
+      >
+        <Container
+          sx={{
+            display: "flex",
+            justifyContent: "flex-end",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <h2>Height: </h2>
+        </Container>
+        <Container
+          sx={{
+            display: "flex",
+            justifyContent: "flex-start",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <h2>{user.height} cm</h2>
+        </Container>
+      </Container>
+      <Container
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "row",
+          alignItems: "center",
+          width: "30%",
+        }}
+        className="info-container"
+      >
+        <Container
+          sx={{
+            display: "flex",
+            justifyContent: "flex-end",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <h2>Newsletter: </h2>
+        </Container>
+        <Container
+          sx={{
+            display: "flex",
+            justifyContent: "flex-start",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <h2>{user.newsletter}</h2>
+        </Container>
+      </Container>
+      <Container sx={{justifyContent:'center', alignItems: 'center', display: 'flex', marginTop: '3vh'}}>
+      <Button variant="contained" style={{backgroundColor: 'darkblue', color: 'white'}}>Edit Profile</Button>      </Container>
+    </div>
+  );
 };
 
 export default UserProfile;
